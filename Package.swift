2fb00e76988c0fd494b1724ee76112acf45f1f2d
Valentin <radu.v.valentin@me.com>
2020-03-21 15:19:12 +0200

    Added minimum platform dependencies

--- Package.swift
+++ Package.swift
@@ -6,7 +6,8 @@ import PackageDescription
 let package = Package(
     name: "Pathcut",
     platforms: [
-       .macOS(.v10_15)
+        .macOS(.v10_15),
+        .iOS(.v11)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
